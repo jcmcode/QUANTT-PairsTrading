6f606d891bf6509ba836d6dda39c1e3cdbf2e97a
Kevin Yang <kevinyang873@gmail.com>
2025-11-04 17:41:26 -0500

    Hedge

--- trading_part1/hedge_ratio.py
+++ trading_part1/hedge_ratio.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import statsmodels.api as sm
 
+
 def calculate_spread(df, ticker1, ticker2, hedge_ratio):
     
     spread = df[ticker1] - (hedge_ratio * df[ticker2])
@@ -23,6 +24,27 @@ def rolling_std_calc(spread, windows=(30, 60, 252)):
         table[f"{w}daystd"]  = spread.rolling(w, min_periods=w).std()
     return table
 
+def hedge(df):
+    # dependent variable 
+    y = df["KO"]
+    # independent variable, need the sm.add_constant or else it would go through the origin and have no intercepts
+    X = sm.add_constant(df["PEP"])
+
+    # Build and fit the OLS regression
+    model = sm.OLS(y, X)
+    results = model.fit()
+
+    # Pull out the coefficients
+    alpha = results.params["const"]
+    # stores hedge ratio
+    beta  = results.params["PEP"]
+    print(f'Hedge Ratio: {beta}')
+
+    # print regression analysis
+    print(results.summary())
+
+    return beta
+
 # load the file and read date column
 df = pd.read_csv("data/KO_PEP.csv", parse_dates=["Date"])
 # sort by date
@@ -30,23 +52,7 @@ df = df.set_index("Date").sort_index()
 # drop missing values
 df = df[["KO", "PEP"]].dropna()
 
-# dependent variable 
-y = df["KO"]
-# independent variable, need the sm.add_constant or else it would go through the origin and have no intercepts
-X = sm.add_constant(df["PEP"])
-
-# Build and fit the OLS regression
-model = sm.OLS(y, X)
-results = model.fit()
-
-# Pull out the coefficients
-alpha = results.params["const"]
-# stores hedge ratio
-beta  = results.params["PEP"]
-print(f'Hedge Ratio: {beta}')
-
-# print regression analysis
-print(results.summary())     
+hedge_ratio = hedge(df)
 
 # spread
 spread = calculate_spread(df, "KO", "PEP", beta)
