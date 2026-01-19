ae3dc92d9e2a77bfd4992e6e7e8dcfeb5fada410
jcmcode <147959838+jcmcode@users.noreply.github.com>
2026-01-19 15:01:00 -0500

    deleted

--- electron-dashboard/public/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  analyzePairs: (data) => ipcRenderer.invoke('analyze-pairs', data),
-  getBacktestResults: (params) => ipcRenderer.invoke('get-backtest-results', params),
-  onDataUpdate: (callback) => ipcRenderer.on('data-update', callback),
-  removeDataUpdateListener: () => ipcRenderer.removeAllListeners('data-update')
-});
